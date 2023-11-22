3c15a6d9ab997ebc7eb47b79aec6d5a101f02183
Anusraza09 <sanus8339@gmail.com>
2023-11-22 22:14:30 +0600

    first commit

---
+++ aap.js
@@ -0,0 +1,227 @@
+var num1=3;
+var num2=5;
+alert(num1 + num2)
+document.write("Sum of 3 and 5 is 8 <br/>")
+
+var num1=3;
+var num2=5;
+alert(num1 - num2)
+
+var num1=3;
+var num2=5;
+alert(num1 * num2)
+
+var num1=3;
+var num2=5;
+alert(num1 / num2)
+
+
+
+var a = 5;
+var b = a++
+var c = 7
+var d = c--
+var bowser = ["intial value: 5","value after increment is: 6","Value after addition is: 13", "value after decrement is:12"]
+alert(
+    a + c
+
+)
+document.write( bowser +"</br>" )
+document.write(a + c + "</br>")
+
+
+
+
+var ticketPrice = "ticket Price" + " 600 PKR"
+var ticket = " Buy tickets" + " 5 |"
+var calculate = " total price 3000PKR"
+var total = calculate 
+
+alert(ticketPrice)
+alert(ticket + calculate)
+alert(calculate)
+document.write(ticketPrice)
+document.write(ticket)
+document.write(calculate) 
+
+
+
+
+
+var table = prompt ("enter table number 4")
+document.write("Table of "+ table +"<br>")
+document.write(table + "x" + 1 + "="+ table*1 +"<br>")
+document.write(table + "x" + 2 + "="+ table*2 +"<br>")
+document.write(table + "x" + 3 + "="+ table*3 +"<br>")
+document.write(table + "x" + 4 + "="+ table*4 +"<br>")
+document.write(table + "x" + 5 + "="+ table*5 +"<br>")
+document.write(table + "x" + 6 + "="+ table*6 +"<br>")
+document.write(table + "x" + 7 + "="+ table*7 +"<br>")
+document.write(table + "x" + 8 + "="+ table*8 +"<br>")
+document.write(table + "x" + 9 + "="+ table*9 +"<br>")
+document.write(table + "x" + 10 + "="+ table*10 +"<br>")
+
+
+
+
+
+
+var priceItem1 = 650;
+var priceItem2 = 100;
+var quantityItem1 = 1;
+var quantityItem2 = 2;
+var shippingCharges = 100;
+var totalCost = 2750;
+
+// // Calculate total cost
+
+var totalItem1 = priceItem1;
+var totalItem2 = priceItem2;
+document.write("<h2>Receipt</h2>");
+document.write("<p>Item 1: " + totalItem1);
+document.write("<p>Quantity of item 1 is 3</p>");
+document.write("<p>Item 2: " + totalItem2);
+document.write("<p>Quantity of item 2 is 7</p>");
+document.write("<p>Shipping Charges: " + shippingCharges);
+document.write("<p>Total cost of your order is " + totalCost);
+
+
+
+
+var totalMarks = 980;
+ // You can set the total marks here
+var marksObtained = 804;
+ // You can set the marks obtained here
+
+// Calculate percentage
+var percentage = (marksObtained / totalMarks) * 100;
+
+// Display result in the browser
+document.write("<h2>Result</h2>");
+document.write("<p>Total Marks: " + totalMarks + "</p>");
+document.write("<p>Marks Obtained: " + marksObtained + "</p>");
+document.write("<p>Percentage: " + percentage+ "%</p>");
+
+
+
+
+var usDollars = 10;
+var saudiRiyals = 25;
+
+// Exchange rates
+var usdToPkrRate = 104.80;
+var sarToPkrRate = 28;
+
+// Convert to Pakistani Rupees in a single expression
+var totalInPkr = (usDollars * usdToPkrRate) + (saudiRiyals * sarToPkrRate);
+
+// Display result in the browser
+document.write("<h2>Currency Conversion</h2>");
+document.write("<p>Total in Pakistani Rupees: PKR " + totalInPkr+ "</p>");
+
+
+
+
+
+
+
+ // Initialize a variable with some number
+ var initialNumber = 5; // You can set any initial number here
+
+ // Perform arithmetic operations in a single expression
+ var result = (((initialNumber + 5) * 10) / 2);
+
+ // Display result in the browser
+ document.write("<h2>Arithmetic Operations</h2>");
+ document.write("<p>Initial Number: " + initialNumber + "</p>");
+ document.write("<p>Result: " + result + "</p>");
+
+
+
+
+ // Store the current year in a variable
+ var currentYear = 2016;
+
+ // Store their birth year in a variable (replace 1990 with the actual birth year)
+ var birthYear = 1992;
+
+ // Calculate their possible ages based on the stored values
+ var age1 = currentYear - birthYear;
+  // Second possible age if their birthday hasn't occurred yet this year
+
+ // Display the result in the browser
+ document.write("<h2>Age Calculator</h2>");
+ document.write("<p>Current Year: " + currentYear + "</p>");
+ document.write("<p>Birth Year: " + birthYear + "</p>");
+ document.write("<p>your age is " + age1 + " or " + " years old.</p>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
